23f3f4a2dee029f327bcfbb94f5c7251a1a19f67
Song Young-Jin <myevan@outlook.com>
2023-04-09 19:01:51 +0900

    앱 설정 형식 변경: JSON -> YAML

--- RpgApi/Program.cs
+++ RpgApi/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.Extensions.Configuration.Yaml;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddYamlFile("AppSettings.yaml");
+
 var app = builder.Build();
+app.Logger.LogDebug("start");
 
 app.MapGet("/", () => "Hello World!");
 
