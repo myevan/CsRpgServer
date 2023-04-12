5dcc00586cf3067d810d60cfbadcb6d859302612
Song Young-Jin <myevan@outlook.com>
2023-04-12 22:46:48 +0900

    ConfigHelper: ConfigrationManager 에서 Config 생성

---
+++ RpgApi/Helpers/ConfigHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+
+namespace Rpg.Helpers
+{
+    public static class ConfigHelper
+    {
+        public static T Create<T>(ConfigurationManager cfgMgr, string prefix = "") where T : class, new()
+        {
+            var newCfg = new T();
+            return Merge(ref newCfg, cfgMgr, prefix);
+        }
+
+        public static T Merge<T>(ref T refCfg, ConfigurationManager cfgMgr, string prefix="") where T : class
+        {
+            var type = refCfg.GetType();
+                
+            foreach (var field in type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+            {
+                var cfgKey = prefix + field.Name;
+                var cfgVal = cfgMgr[cfgKey];
+
+                if (cfgVal != null)
+                {
+                    var fieldType = field.FieldType;
+                    var value = Convert.ChangeType(cfgVal, fieldType);
+                    field.SetValue(refCfg, value);
+                }
+            }
+
+            foreach (var prop in type.GetProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+            {
+                var cfgKey = prefix + prop.Name;
+                var cfgVal = cfgMgr[cfgKey];
+                if (cfgVal != null)
+                {
+                    var propertyType = prop.PropertyType;
+                    var value = Convert.ChangeType(cfgVal, propertyType);
+                    prop.SetValue(refCfg, value);
+                }
+            }
+            return refCfg;
+        }
+    }
+}
